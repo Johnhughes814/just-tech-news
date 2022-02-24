d7106d547674cbc2db3f541a3f1af270e7d95274
Johnhughes814 <8hughes.john@gmail.com>
2022-02-24 12:15:23 -0600

    blah

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,4 +4,9 @@ const userRoutes = require('./user-routes.js');
 
 router.use('/users', userRoutes);
 
+const postRoutes = require('./post-routes');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+
 module.exports = router;
